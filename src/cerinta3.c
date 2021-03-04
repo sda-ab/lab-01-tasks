d536552c164183e339e9bf5b0c462599e455ed6a
Udroiu Ruben <47859590+UdroiuRuben@users.noreply.github.com>
2021-03-04 14:44:50 +0200

    Update cerinta3.c

--- src/cerinta3.c
+++ src/cerinta3.c
@@ -12,11 +12,11 @@ void realocare_memorie (student **vector, int dimensiune) {
 /**
  * //TODO: Implementarea metodei ,,add_student" (se adauga un nou student pe pozitia poz)
  * Hint:
-	- datele pentru noul student sunt citite in aceasta functie, de la tastatura
 	- optional: se poate utiliza functia swap implementata la cerinta1
  * @param vector  - pointer catre vectorul de studenti 
  * @param poz - pozitia pe care trebuie adaugat noul student
  * @param dimensiune - dimensiunea vectorului
+ * @param stud - reprezinta studentul care trebuie adaugat in vector (acesta are datele necesare deja completate -nume,prenume,etc-)
  */
 void adaugare_student (student *vector, int poz, int dimensiune, student stud) {
 
